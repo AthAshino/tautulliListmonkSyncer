bed98afa4015ea4c8ba942a517594e3df4974690
Thomas <thomasclem33@gmail.com>
2023-12-15 22:46:27 +0100

    ✨ Add listmonk generated client

---
+++ listmonk_client/api/__init__.py
@@ -0,0 +1,18 @@
+# flake8: noqa
+
+# import apis into api package
+from listmonk_client.api.admin_api import AdminApi
+from listmonk_client.api.bounces_api import BouncesApi
+from listmonk_client.api.campaigns_api import CampaignsApi
+from listmonk_client.api.import_api import ImportApi
+from listmonk_client.api.lists_api import ListsApi
+from listmonk_client.api.logs_api import LogsApi
+from listmonk_client.api.maintenance_api import MaintenanceApi
+from listmonk_client.api.media_api import MediaApi
+from listmonk_client.api.miscellaneous_api import MiscellaneousApi
+from listmonk_client.api.public_api import PublicApi
+from listmonk_client.api.settings_api import SettingsApi
+from listmonk_client.api.subscribers_api import SubscribersApi
+from listmonk_client.api.templates_api import TemplatesApi
+from listmonk_client.api.transactional_api import TransactionalApi
+
